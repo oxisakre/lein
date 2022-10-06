4c28cd01a8679f91750aa11a7f80c3549b5d5f9a
oxisakre <leoalvarg@gmail.com>
2022-10-06 20:13:29 +0200

    fixing messages errors

--- leo_book/nucleus/admin.py
+++ leo_book/nucleus/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+admin.site.register(Profile) #Para ver el modelo 'Profile' en el admin panel de la pagina
--- leo_book/nucleus/views.py
+++ leo_book/nucleus/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import email
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+from nucleus.models import Profile
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username'] #devuelve el username
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2: #chequear datos
+            if User.objects.filter(email=email).exists(): 
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password) #crea el usuario
+                user.save()
+
+                #logear al usuario y redirigirlo a la pagina de configuracion
+                # crear un perfil para el usuario nuevo
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords Not Matching')
+            return redirect('signup')
+    else:    
+        return render(request, 'signup.html')
