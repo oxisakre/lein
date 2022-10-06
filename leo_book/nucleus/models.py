b97b8ac5b515ed6f04a6ebc80291a9b151c42d7e
oxisakre <leoalvarg@gmail.com>
2022-10-06 18:46:57 +0200

    adding user

--- leo_book/nucleus/models.py
+++ leo_book/nucleus/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # usamos el User como foreingkey
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='generic-profile_0.png') #el upload genera una carpeta automatica si no la hay
+    location = models.CharField(max_length=100, blank=True)
+
+
+def __str__(self):
+    return self.user.username 
