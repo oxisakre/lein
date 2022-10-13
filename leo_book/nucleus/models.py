2f9d4f232965cc240deb5623f4673e943c16fbe4
oxisakre <leoalvarg@gmail.com>
2022-10-13 21:34:40 +0200

    add

--- leo_book/nucleus/models.py
+++ leo_book/nucleus/models.py
@@ -1,5 +1,8 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid # para general id unicas
+from datetime import datetime
 
 User = get_user_model()
 # Create your models here.
@@ -12,5 +15,16 @@ class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
 
 
-def __str__(self):
-    return self.user.username 
+    def __str__(self):
+        return self.user.username 
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4) # dar una id unica
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
